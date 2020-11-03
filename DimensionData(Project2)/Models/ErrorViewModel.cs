b38edf3b04242b588f1d66754f3330eb3669d2be
thandi dlamini <motladiledlamini@gmail.com>
2020-11-03 16:45:32 +0200

    dir

---
+++ DimensionData(Project2)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DimensionData_Project2_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

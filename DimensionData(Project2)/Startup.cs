0d77e6b2b1d83e42b59a0e444c7cc2364f0cc4ae
thandi dlamini <motladiledlamini@gmail.com>
2020-11-17 16:05:18 +0200

    service configure

--- DimensionData(Project2)/Startup.cs
+++ DimensionData(Project2)/Startup.cs
@@ -33,6 +33,7 @@ namespace DimensionData_Project2_
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddDbContext<DimensionDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));  
             services.AddRazorPages();
         }
 

231ad6efe4598915b8fdd7e095b89cdf7ad5d2fe
thandi dlamini <motladiledlamini@gmail.com>
2020-11-19 12:05:59 +0200

    Identity

---
+++ DimensionData(Project2)/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DimensionData_Project2_.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DimensionData_Project2_.Areas.Identity.IdentityHostingStartup))]
+namespace DimensionData_Project2_.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
